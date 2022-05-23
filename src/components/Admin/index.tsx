9a9c3e4e117d812084078b03d64172c67876e9ec
Vinicius Pacheco <vinicius@pachecoof@hotmail.com>
2022-05-23 19:55:06 -0300

    Ajuste página admin (desafio alura

--- src/components/Admin/index.tsx
+++ src/components/Admin/index.tsx
@@ -1,11 +1,10 @@
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 
 function Admin() {
     const { user } = useParams();
-    const navigate = useNavigate();
 
     if (user !== 'Admin') {
-        return <Navigate to="/" />;
+        return <Navigate to="/" />; //Redirecionar para outra página
     }
 
     return <div>Página de Admin</div>;
